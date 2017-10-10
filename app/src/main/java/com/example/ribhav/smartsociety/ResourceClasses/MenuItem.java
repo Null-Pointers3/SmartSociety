3128063857479d0983ed6fd6b6b4946447b81b84
Nimit Arora <nimit17arora@gmail.com>
2017-10-10 19:46:10 +0530

    Recycler View

---
+++ app/src/main/java/com/example/ribhav/smartsociety/ResourceClasses/MenuItem.java
@@ -0,0 +1,17 @@
+package com.example.ribhav.smartsociety.ResourceClasses;
+
+/**
+ * Created by Nimit Arora on 10/10/2017.
+ */
+
+public class MenuItem {
+    private int ImageId;
+
+    public MenuItem(int utilities) {
+        this.ImageId=utilities;
+    }
+
+    public int getImageId() {
+        return ImageId;
+    }
+}
