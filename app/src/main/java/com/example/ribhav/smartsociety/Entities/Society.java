37a93908a13e9724fe2ac4d3577978cc7417968e
Kartik <kartikkwatra2016@gmail.com>
2017-10-10 11:49:30 +0530

    Added user class

---
+++ app/src/main/java/com/example/ribhav/smartsociety/Entities/Society.java
@@ -0,0 +1,13 @@
+package com.example.ribhav.smartsociety.Entities;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Nitish Kumar on 10-10-2017.
+ */
+
+public class Society {
+    private String mSocietyName;
+    private int mPinCode;
+    private ArrayList<User>;
+}
