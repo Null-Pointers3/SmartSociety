7cd9a5b9e720ce997a504d9ee499d84855eed4b6
Nimit Arora <nimit17arora@gmail.com>
2017-10-10 13:19:53 +0530

    Added User

--- app/src/main/java/com/example/ribhav/smartsociety/Entities/User.java
+++ app/src/main/java/com/example/ribhav/smartsociety/Entities/User.java
@@ -5,4 +5,32 @@ package com.example.ribhav.smartsociety.Entities;
  */
 
 public class User {
+    private String userName;
+    private String flatNumber;
+    private int pincode;
+    private Society society;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFlatNumber() {
+        return flatNumber;
+    }
+
+    public void setFlatNumber(String flatNumber) {
+        this.flatNumber = flatNumber;
+    }
+
+    public int getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(int pincode) {
+        this.pincode = pincode;
+    }
 }
